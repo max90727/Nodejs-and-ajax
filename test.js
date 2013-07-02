66593b844eb833db6c8ce0ffa6e113de2d89f62b
Danqing NI <ndq880815@hotmail.com>
2013-07-02 08:50:32 +0200

    Update test.js

--- test.js
+++ test.js
@@ -2,15 +2,15 @@ function execute(receivedData) {
   $( '#result' ).append('<h3>Show Ajax Result</h3><br>');
 	console.log(receivedData);
 	//$( '#result' ).append('<p>'+ receivedData +'</p>');
-		
 }
 
-function ajaxSetup() {
+function ajaxSetup(country_path) {
+	var path = {filePath: country_path };
 	$.ajax({
-		url: "http://localhost:3000/ajax/EU&callback=?",
+		url: "http://localhost:3000/ajax",
     		type: "GET",
-    		dataType: "jsonp",
-		//contentType: "application/json",
+    		dataType: "json",
+		data: path,
 		cache: false,
 		timeout: 5000,
 		success: function(data) {
@@ -19,10 +19,28 @@ function ajaxSetup() {
 	});
 }
 
+function ajaxJSONPSetup() {
+	$.ajax({
+		url: "http://localhost:3000/JSONP?cb=?",
+    		type: "GET",
+    		dataType: "jsonp",
+		cache: 'false',
+		jsonp: 'cb',
+		timeout: 5000,
+		success: function(data) {
+			execute(data);
+			},
+		error: function() {
+				console.log("a oh!");
+			}
+	});
+}
+
 
 $( document ).ready(function() {
 	//to do
 	$('#ajax_test').click(function() {
-		ajaxSetup();
+		//ajaxSetup();		//use json type
+		ajaxJSONPSetup();	////use jsonp type
 	});
 });
